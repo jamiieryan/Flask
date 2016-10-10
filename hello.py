bce8757b6039478cdd397d95078871e6ba45563b
jamiieryan <g00317177@gmit.ie>
2016-10-10 10:39:08 +0100

    step four

--- hello.py
+++ hello.py
@@ -3,7 +3,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-	return "Hello World!"
+	return "Index Page!"
+
+@app.route("/name")
+def name():
+	return "Your name is:"
+
+@app.route("/name/<jamie>")
+def name_two(jamie):
+	return "Your name is:" + jamie
 
 if __name__ == "__main__":
 	app.run()
